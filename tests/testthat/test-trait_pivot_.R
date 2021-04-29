67ba455818d26b497077073b1367341fba9ef76a
Fonti Kar <f.kar@unsw.edu.au>
2021-04-29 15:35:00 +1000

    Fixed errors in test file

--- tests/testthat/test-trait_pivot_.R
+++ tests/testthat/test-trait_pivot_.R
@@ -1,11 +1,10 @@
-library(austraits.R)
 data <- austraits$traits %>% 
-  filter(dataset_id == "Falster_2003")
+  dplyr::filter(dataset_id == "Falster_2003")
 
 #Should run smoothly
-expect_silent(traits_spread <- spread_trait_data(data))
+expect_silent(traits_spread <- trait_pivot_wider(data))
 
 #Should throw error
-expect_error(spread_trait_data(austraits), label = "The austraits object is a list. Try austraits$trait")
-expect_error(spread_trait_data(austraits$sites), label = "Relevent column names from trait data frame not found")
-expect_error(spread_trait_data(austraits$methods), label = "Relevent column names from trait data frame not found")
+expect_error(trait_pivot_wider(austraits), label = "The austraits object is a list. Try austraits$trait")
+expect_error(trait_pivot_wider(austraits$sites), label = "Relevent column names from trait data frame not found")
+expect_error(trait_pivot_wider(austraits$methods), label = "Relevent column names from trait data frame not found")
