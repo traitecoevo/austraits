a55d56c9ab977bc11de9762f3284eccd4d459284
Fonti Kar <f.kar@unsw.edu.au>
2021-04-29 10:28:58 +1000

    Rename spread to pivot, added roxygen2 for trait_pivot_wider

---
+++ R/trait_pivot_wider.R
@@ -0,0 +1,31 @@
+#' @title Pivot long format ausTrait data into a wide format
+#'
+#' @description trait_pivot_wider "widens" long format data ("tidy data")
+#' Long format data has measurements on different rows, and just a single column which denotes what type of measurement it is.
+#' This function manipulates the data frame into wide format has data so that each trait in it's own column.
+#' @usage trait_pivot_wider(data)
+#' @param data A data object g 
+#' @return data
+#' @export
+#'
+#' @examples 
+#' data <- austraits$traits %>% 
+#' filter(dataset_id == "Falster_2003")
+#' data #long format 
+#' traits_wide <- trait_pivot_wider(data) 
+#' traits_wide #wide format
+#' @author Daniel Falster - daniel.falster@unsw.edu.au
+#' 
+trait_pivot_wider <- function(data) {
+  
+  vars <- c("value", "unit", "value_type", "replicates")
+  ret <- list()
+  for(v in vars) {
+    ret[[v]] <- data %>% 
+      rename(to_spread = !!v) %>%
+      select(dataset_id, taxon_name, site_name, observation_id, trait_name, to_spread, original_name) %>%
+      pivot_wider(names_from = trait_name, values_from = to_spread)
+  }
+  
+  ret
+}
