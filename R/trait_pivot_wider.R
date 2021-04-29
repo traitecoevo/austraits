44183c808509d3ad55e3155876a31ea290b53ebe
Fonti Kar <f.kar@unsw.edu.au>
2021-04-29 10:53:25 +1000

    Added documentation for trait_pivot_wider

--- R/trait_pivot_wider.R
+++ R/trait_pivot_wider.R
@@ -1,19 +1,21 @@
 #' @title Pivot long format ausTrait data into a wide format
 #'
 #' @description trait_pivot_wider "widens" long format data ("tidy data")
-#' Long format data has measurements on different rows, and just a single column which denotes what type of measurement it is.
-#' This function manipulates the data frame into wide format has data so that each trait in it's own column.
+#' Long format data has measurements on different rows and the type of measurement denoted in a single column.
+#' This function manipulates the data into wide format so that each trait in it's own column.
 #' @usage trait_pivot_wider(data)
-#' @param data A data object g 
+#' @param data A data object generated from ausTraits - see example
 #' @return data
 #' @export
 #'
 #' @examples 
+#' \dontrun{
 #' data <- austraits$traits %>% 
 #' filter(dataset_id == "Falster_2003")
 #' data #long format 
 #' traits_wide <- trait_pivot_wider(data) 
 #' traits_wide #wide format
+#' }
 #' @author Daniel Falster - daniel.falster@unsw.edu.au
 #' 
 trait_pivot_wider <- function(data) {
