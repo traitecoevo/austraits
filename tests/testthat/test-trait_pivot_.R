2651f33765e40c3729ee6fcc5ccf2dfb51df4810
Fonti Kar <f.kar@unsw.edu.au>
2021-05-14 12:08:18 +1000

    added to the pivot testing script

--- tests/testthat/test-trait_pivot_.R
+++ tests/testthat/test-trait_pivot_.R
@@ -1,16 +1,32 @@
+#Pull in data
 data(austraits)
+#Filter some data
 data <- dplyr::filter(austraits$traits, dataset_id == "Falster_2003")
+wide_data <- austraits::trait_pivot_wider(data)
 
-#Should run smoothly
-expect_silent(trait_pivot_wider(data))
-#expect_silent(trait_pivot_wider(data) %>% trait_pivot_longer()) This produces messages but I can't see them?! 
+test_that("function shouldn't complain and throw errors", {
+  expect_silent(trait_pivot_wider(data))
+  #This test is producing messages in checks but not when tested interactively
+  #expect_silent(trait_pivot_longer(wide_data)) 
+})
 
-#Should throw error
+test_that("functions should throw error when provided wrong input", {
 expect_error(trait_pivot_wider(austraits), label = "The austraits object is a list. Try austraits$trait")
 expect_error(trait_pivot_wider(austraits$sites), label = "Relevent column names from trait data frame not found")
 expect_error(trait_pivot_wider(austraits$methods), label = "Relevent column names from trait data frame not found")
+expect_error(trait_pivot_longer(austraits), label = "Cannot gather an already long format dataframe")
+})
 
-#Structural tests
+test_that("input and output are of expected structure", {
 expect_match(class(trait_pivot_wider(data)), "list")
+expect_equal(class(trait_pivot_longer(wide_data)), c("tbl_df","tbl","data.frame"))
 expect_equal(trait_pivot_wider(data) %>% length, 5)
+})
+
+test_that("before and after pivots match", {
+  # dim(data)
+  # dim(trait_pivot_longer(wide_data))
+  # names(data) 
+  # names(trait_pivot_wider(data)[[1]])
+})
 
