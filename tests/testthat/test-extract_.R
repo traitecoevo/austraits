63424fc9f775c9cc807cb80711f7cf0a72b7c5af
Fonti Kar <f.kar@unsw.edu.au>
2021-05-18 15:15:17 +1000

    Added some tests for etract dataset

--- tests/testthat/test-extract_.R
+++ tests/testthat/test-extract_.R
@@ -2,7 +2,8 @@
 data(austraits)
 
 #Extract a dataset
-subset <- extract_dataset(austraits, "Falster_2003")
+dataset_id <- c("Falster_2003")
+subset <- extract_dataset(austraits, dataset_id = dataset_id)
 
 test_that("extracted dataset has some structure as austraits build", {
   expect_equal(length(austraits), length(extract_dataset(austraits, "Falster_2003")))
@@ -11,4 +12,7 @@ test_that("extracted dataset has some structure as austraits build", {
   expect_named(austraits, names(subset))
 })
 
-test_that("extraction of dataset was successful")
+test_that("extraction of dataset was successful", {
+  expect_match(dataset_id, unique(subset$traits$dataset_id))
+  expect_equal(1, dplyr::n_distinct(subset$traits$dataset_id))x
+})
