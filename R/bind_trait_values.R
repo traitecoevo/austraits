7b402253bbd437072c71be8d0ebf12cb542eb276
Fonti Kar <f.kar@unsw.edu.au>
2021-05-19 14:24:56 +1000

    Added bind_ seperate_ and a helper function

---
+++ R/bind_trait_values.R
@@ -0,0 +1,43 @@
+#' @title Bind trait values
+#'
+#' @description This function condenses data for studies that have multiple observations for a given trait into a single row. 
+#' This function concatenates multiple values into a single cell
+#' @usage bind_trait_values(austraits$traits)
+#' @param data The trait data frame generated from austraits - see example
+#' @return tibble that is condensed down where multiple observations in value, value_type and replicates are collapsed down and separated by '--' 
+#'
+#' @examples 
+#' \dontrun{
+#' traits <- austraits$traits %>% 
+#' dplyr::filter(dataset_id == "Falster_2005_1")
+#' traits
+#' traits_bind <- bind_trait_values(traits)
+#' }
+#' @author Daniel Falster - daniel.falster@unsw.edu.au
+#' @export
+#' @importFrom rlang .data
+
+bind_trait_values <- function(data) {
+  
+  bind_x <- function(x) paste0(x, collapse = "--")
+  
+  bind_values_worker <- function(.data) {
+    # If more than one value per group need to combine
+    if(nrow(.data) > 1) {
+      return(
+        .data %>% 
+          dplyr::mutate(value = bind_x(value),
+                        value_type = bind_x(value_type),
+                        replicates = bind_x(replicates)) %>%
+          dplyr::filter(dplyr::row_number()==1) 
+      )
+    }
+    .data
+  }
+  
+  data  %>% 
+    dplyr::group_by(observation_id, trait_name) %>% 
+    bind_values_worker() %>% 
+    dplyr::ungroup() %>% 
+    dplyr::arrange(observation_id, trait_name, value_type)
+}
---
+++ R/seperate_trait_values.R
@@ -0,0 +1,56 @@
+#' @title Separate bounded trait values
+#'
+#' @description This function reverts the action of bind_trait_values. 
+#' This function separates values that were concatenated so that studies that have multiple observations for a given trait will have seperate row for each observation.
+#' @usage separate_trait_values(data, definitions)
+#' @param data The trait data frame generated from austraits - see example
+#' @param definitions The austraits definitions data frame
+#' @return trait tibble
+#' @examples 
+#' \dontrun{
+#' traits <- austraits$traits %>% 
+#' dplyr::filter(dataset_id == "Falster_2005_1")
+#' traits
+#' traits_bind <- bind_trait_values(traits)
+#' separate_trait_values(traits_bind)
+#' }
+#' @author Daniel Falster - daniel.falster@unsw.edu.au
+#' @export
+#' @importFrom rlang .data
+#' 
+separate_trait_values <- function(data, definitions) {
+  
+  separate_x <- function(x) strsplit(x, "--")[[1]]
+  
+  separate_values_worker <- function(df) {
+    
+    df[rep(1, df$n_vals[1]),] %>%
+      dplyr::mutate(
+        value = separate_x(value[1]),
+        value_type = separate_x(value_type[1]),
+        replicates = separate_x(replicates[1])
+      )
+  }
+  
+  
+  # record the number of values in each row of data
+  data$n_vals <- 1 + stringr::str_count(data$value_type, "--")
+  
+  # separate out those rows requiring no modification
+  out_1 <- data %>% 
+    dplyr::filter(n_vals == 1)
+  
+  # separate out those rows requiring modification & modify
+  out_2 <- data %>% 
+    dplyr::filter(n_vals > 1) %>% 
+    split(paste(.$observation_id, .$trait_name)) %>%    
+    lapply(separate_values_worker) %>% 
+    dplyr::bind_rows()
+  
+  # join it all back together, clean up and sort as in original
+  dplyr::bind_rows(out_1, out_2) %>% 
+    dplyr::select(-n_vals) %>% 
+    dplyr::mutate(replicates = clean_NA(replicates),
+                  value_type = factor(clean_NA(value_type), levels = names(definitions$value_type$values))) %>% 
+    dplyr::arrange(observation_id, trait_name, value_type)
+}
