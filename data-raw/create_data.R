a15075154d4b52f481dcc297964f84d95032372a
Daniel Falster <daniel.falster@unsw.edu.au>
2021-05-20 15:56:10 +1000

    Update data to include all references
    - previously was missing some of the secondary references from Zanne_2009

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -1,10 +1,9 @@
 
-## code to prepare `DATASET` dataset goes here
+## code to prepare an example dataset, that go public
 
-library(tidyverse)
-source("R/usage.R")
+devtools::load_all()
 
-austraits_all <- readRDS("../austraits/ignore/austraits-2.1.0.rds")
+austraits_all <- readRDS("../austraits.build/export/data/austraits_3.0.0.rds")
 dataset_id <- c("Falster_2003", "Falster_2005_1", "Falster_2005_2", "Zanne_2009")
 
 austraits <- extract_dataset(austraits_all, dataset_id)
