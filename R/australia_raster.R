68f4ebce17766ab522dae05422717d2bc76c26fa
Fonti Kar <f.kar@unsw.edu.au>
2021-06-15 12:08:30 +1000

    Replaced old workflow for plotting site plots with raw raster file, upadloed raster df instead

---
+++ R/australia_raster.R
@@ -0,0 +1,6 @@
+#' A dataframe containing raster coordinates of Australia
+#'
+#' @description A dataframe containing raster coordinates of Australia, used in plot_site_locations
+#' @format 250000 obs 
+#' @source see data-raw create_raster.R
+"australia_raster"
--- R/plot_site_locations.R
+++ R/plot_site_locations.R
@@ -4,19 +4,23 @@
 #' @param feature grouping/classification categories
 #' @author Dony Indiarto - d.indiarto@student.unsw.edu.au
 #' @return ggplot of sites
-#' @importFrom ggplot2 ggplot aes geom_raster theme scale_fill_grey xlab ylab facet_wrap
 #' @examples 
 #' \dontrun{
-#' data <- austraits %>% join_all() %>% `[[`("traits") 
-#' data %>% plot_site_locations()
+#' #All traits from a given study
+#' data <- austraits %>% extract_dataset(dataset_id = "Falster_2003") %>% join_all() 
+#' data$traits %>% plot_site_locations()
 #' 
-#' data <- austraits %>% extract_trait(trait_names = c("plant_height")) %>% join_all() %>% `[[`("traits")
-#' data %>% plot_site_locations("trait_name")
+#' #Single trait
+#' data <- austraits %>% extract_trait(trait_names = c("plant_height")) %>% join_all() 
+#' data$traits %>% plot_site_locations("trait_name")
 #' }
 #' @export
 #' @importFrom rlang .data
-plot_site_locations <- function(traits, feature="trait_name", size=0.5, alpha = 0.8, xlab = "", ylab=""){
+#' @importFrom ggplot2 ggplot aes geom_raster theme scale_fill_grey xlab ylab facet_wrap element_line element_blank element_rect unit
 
+plot_site_locations <- function(traits, feature="trait_name", size=0.5, alpha = 0.8, xlab = "", ylab=""){
+  
+  #Create site data
   sites <- 
     traits %>%
     dplyr::select(.data$site_name, .data$`latitude (deg)`, .data$`longitude (deg)`, !!feature) %>%
@@ -25,17 +29,15 @@ plot_site_locations <- function(traits, feature="trait_name", size=0.5, alpha = 
     dplyr::filter(
       .data$`latitude (deg)` > (-45), .data$`latitude (deg)` < (-9.5),
       .data$`longitude (deg)` > (110), .data$`longitude (deg)` < (153))
-  
+
+  #Create site map  
   site_map <- 
     ggplot() +
-    # base map
-    geom_raster(data = raster::as.data.frame(austraits::australia_map_raster, xy = T), 
-                aes(x = x, y = y,fill = factor(australia))
-    )  +
-    # add data
+    geom_raster(data = austraits::australia_raster, aes(x = .data$x, y = .data$y, fill = .data$ID), show.legend = F) +
+    # Add trait data
     ggpointdensity::geom_pointdensity(
       data = sites,
-      aes(y = `latitude (deg)`, x = `longitude (deg)`),
+      aes(y = .data$`latitude (deg)`, x = .data$`longitude (deg)`),
       inherit.aes = FALSE,
       show.legend = TRUE,
       adjust = 1,
@@ -45,27 +47,20 @@ plot_site_locations <- function(traits, feature="trait_name", size=0.5, alpha = 
     viridis::scale_color_viridis(option = "plasma") +
     theme(
       legend.justification = c(-0.1, 0),
-      legend.position = c(0.05, 0.05),
+      legend.position = "bottom",
       legend.direction  = "horizontal",
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.background = element_blank(),
-            panel.border = element_rect(colour = "black", fill=NA, size=1),
-            axis.ticks.length = unit(1, "mm"),
-            axis.ticks = element_line(size = 1)
-    ) +
-    scale_fill_grey(
-      name = "",
-      start = 0.8,
-      guide = FALSE,
-      na.value = "white"
-    ) + xlab(xlab) + ylab(ylab)
+            panel.grid.major = ggplot2::element_blank(),
+            panel.grid.minor = ggplot2::element_blank(),
+            panel.background = ggplot2::element_blank(),
+            panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
+            axis.ticks.length = ggplot2::unit(1, "mm"),
+            axis.ticks = ggplot2::element_line(size = 1)
+    )  + xlab(xlab) + ylab(ylab)
   
-  # facet by feature if specified
+  # facet by feature if specified - default
   if(!is.na(feature)){
       site_map <- site_map + facet_wrap(paste("~", feature))
   }
-  
   site_map
 }
 
