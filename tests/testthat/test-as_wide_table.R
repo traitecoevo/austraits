492f9f3f59be6eccd0104420d1b4132f1a55ed3b
Fonti Kar <f.kar@unsw.edu.au>
2022-03-28 13:23:31 +1100

    Tests for as_wide_table

--- tests/testthat/test-as_wide_table.R
+++ tests/testthat/test-as_wide_table.R
@@ -5,3 +5,15 @@ test_that("Function is working", {
   expect_named(austraits %>% as_wide_table())
   expect_type(austraits %>% as_wide_table(), "list")
 })
+
+test_that("Output is correct", {
+  expect_equal(austraits %>% as_wide_table() %>% nrow(), austraits$traits %>% nrow())
+  expect_gt(austraits %>% as_wide_table() %>% ncol(), expected = austraits$traits %>% ncol())
+})
+
+test_that("Complains at the right time", {
+  expect_error(austraits$traits %>% as_wide_table())
+})
+
+
+  
