199ca58bb4d5051a629166a187b554ad7cd25678
Fonti Kar <f.kar@unsw.edu.au>
2022-10-24 08:30:39 +1100

    Hex sticker files

---
+++ inst/figures/hex.R
@@ -0,0 +1,7 @@
+imgurl <- file.path("inst/figures/austraits_logo.png")
+hexSticker::sticker(imgurl, package="austraits", 
+                    p_color = "#5B5B5B",
+                    p_size=20,s_x=1.05, s_y=1.02,
+                    s_width=.75, s_height = 0.75,
+                    h_fill = "white", h_colour = #3B7F1E",
+                    filename="inst/figures/austraits_hex.png")
