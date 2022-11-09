3f180f6672caff2e4d0cdf58405803ec34d5ea4d
Fonti Kar <f.kar@unsw.edu.au>
2022-11-09 15:03:28 +1100

    Minor edits to hex

--- inst/figures/hex.R
+++ inst/figures/hex.R
@@ -1,9 +1,9 @@
 imgurl <- file.path("inst/figures/austraits_logo_green_transparent.png")
 hexSticker::sticker(imgurl, package="austraits", 
                     p_color = "white",
-                    p_size=30,
-                    p_y = 1,
-                    s_x=.94, s_y=1,
+                    p_size=32,
+                    p_y = 1.05,
+                    s_x=.94, s_y=1.02,
                     s_width=.75, s_height = .75,
                     h_fill = "chartreuse4", h_color = "white",
                     filename="inst/figures/austraits_hex.png")
