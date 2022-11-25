9ee54b1c9776dc1ebfc6f6472d7a3f648c6be748
Fonti Kar <f.kar@unsw.edu.au>
2022-11-25 12:58:26 +1100

    Created another austraits lite for testing new release

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -8,11 +8,21 @@ dataset_id <- c( unique(austraits_all$traits$dataset_id) %>% sample(5), "Falster
 
 austraits_lite <- extract_dataset(austraits_all, dataset_id)
 
+# updated release
+austraits_newrel <- readRDS("ignore/data/austraits/austraits_newrel.rds") 
+attr(austraits_newrel, "class") <- "austraits"
+
+datasets <- c("Crous_2013", "Crous_2019", "Buckton_2019", "Kooyman_2011", "Bloomfield_2018", 
+              "Wright_2019", "Westoby_2014", "Vesk_2019", "Leigh_2003", "Prior_2003",
+              "Prior_2016", "Choat_2006", "Choat_2012", "ABRS_1981")
+
+austraits_lite_post <- austraits_newrel %>% extract_dataset(dataset_id = datasets)
+
 ## code to prepare `australia_map_raster` dataset goes here
 
 australia_map_raster <- raster::raster("ignore/australia.tif") 
 australia_map_raster <- australia_map_raster %>% raster::as.data.frame(xy = T,na.rm=T)
 
-usethis::use_data(austraits_lite, australia_map_raster, internal = TRUE, overwrite = TRUE)
+usethis::use_data(austraits_lite, australia_map_raster, austraits_lite_post, internal = TRUE, overwrite = TRUE)
 
 
