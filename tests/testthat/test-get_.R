b6775676d891bad42cb6068ca61ad0724dd572ad
Fonti Kar <f.kar@unsw.edu.au>
2023-01-12 17:03:06 +1100

    Version testing for extract, and summarise_austriats

--- tests/testthat/test-get_.R
+++ tests/testthat/test-get_.R
@@ -1,34 +1,54 @@
-test_that("Function works", {
-  expect_visible(austraits_lite %>% summarise_austraits("family"))
-  expect_visible(austraits_lite %>% summarise_austraits("genus"))
-  expect_visible(austraits_lite %>% summarise_austraits("trait_name"))
-})
+library(purrr)
 
-test_that("Throws errors", {
-  expect_error(austraits_lite %>% summarise_austraits("observation_id"))
-  expect_error(austraits_lite %>% summarise_austraits("trait"))
-  expect_error(austraits_lite %>% summarise_austraits("unit"))
-  expect_error(austraits_lite %>% summarise_austraits("source"))
-  
-})
+austraits <- list(austraits_lite,
+                 austraits_lite_post)
 
-test_that("Output correct", {
-  family <- austraits_lite %>% summarise_austraits("family")
-  genus <- austraits_lite %>% summarise_austraits("genus")
-  trait_nm <- austraits_lite %>% summarise_austraits("trait_name")
-  
-  expect_length(family, 5)
-  expect_length(genus, 5)
-  expect_length(trait_nm, 5)
-  
-  expect_named(family, expected = c("family", "n_records", "n_dataset", "n_taxa", "percent_total"))
-  expect_named(genus, expected = c("genus", "n_records", "n_dataset", "n_taxa", "percent_total"))
-  expect_named(trait_nm, expected = c("trait_name", "n_records", "n_dataset", "n_taxa", "percent_total"))
-  
-  actual_family <- austraits_lite$taxa$family %>% unique() 
-  actual_genus <- austraits_lite$taxa$genus %>% unique() 
-  
-  expect_equal(nrow(family), actual_family[! is.na(actual_family)] %>% length())
-  expect_equal(nrow(genus), actual_genus[! is.na(actual_genus)] %>% length())
-  expect_equal(nrow(trait_nm), austraits_lite$traits$trait_name %>% unique() %>% length())
-})
+test_summarise <- function(austraits){
+  test_that("Function works", {
+    expect_visible(austraits %>% summarise_austraits("family"))
+    expect_visible(austraits %>% summarise_austraits("genus"))
+    expect_visible(austraits %>% summarise_austraits("trait_name"))
+  })
+}
+
+map(austraits, 
+    ~test_summarise(.x))
+
+test_summarise_errors <- function(austraits){
+  test_that("Throws errors", {
+    expect_error(austraits %>% summarise_austraits("observation_id"))
+    expect_error(austraits %>% summarise_austraits("trait"))
+    expect_error(austraits %>% summarise_austraits("unit"))
+    expect_error(austraits %>% summarise_austraits("source"))
+    
+  })
+}
+
+map(austraits, 
+    ~test_summarise_errors(.x))
+
+test_summarise_output <- function(austraits){
+  test_that("Output correct", {
+    family <- austraits_lite %>% summarise_austraits("family")
+    genus <- austraits_lite %>% summarise_austraits("genus")
+    trait_nm <- austraits_lite %>% summarise_austraits("trait_name")
+    
+    expect_length(family, 5)
+    expect_length(genus, 5)
+    expect_length(trait_nm, 5)
+    
+    expect_named(family, expected = c("family", "n_records", "n_dataset", "n_taxa", "percent_total"))
+    expect_named(genus, expected = c("genus", "n_records", "n_dataset", "n_taxa", "percent_total"))
+    expect_named(trait_nm, expected = c("trait_name", "n_records", "n_dataset", "n_taxa", "percent_total"))
+    
+    actual_family <- austraits_lite$taxa$family %>% unique() 
+    actual_genus <- austraits_lite$taxa$genus %>% unique() 
+    
+    expect_equal(nrow(family), actual_family[! is.na(actual_family)] %>% length())
+    expect_equal(nrow(genus), actual_genus[! is.na(actual_genus)] %>% length())
+    expect_equal(nrow(trait_nm), austraits_lite$traits$trait_name %>% unique() %>% length())
+  })
+}
+
+map(austraits, 
+    ~ test_summarise_output())
