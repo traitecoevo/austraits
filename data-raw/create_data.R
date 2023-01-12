426fc4dc4544aeb7c82ca6248471915a2cad743d
Fonti Kar <f.kar@unsw.edu.au>
2023-01-12 16:52:50 +1100

    Updated code to subset 4.0.0 from zenodo

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -4,14 +4,11 @@
 devtools::load_all()
 set.seed(109)
 austraits_all <- load_austraits(version = "3.0.2", path = "ignore/data/austraits")
-dataset_id <- c( unique(austraits_all$traits$dataset_id) %>% sample(5), "Falster_2003", "Falster_2005_1", "Falster_2005_2" )
-
+dataset_id <- c( unique(austraits_all$traits$dataset_id) %>% sample(5), "Falster_2003", "Falster_2005_1", "Falster_2005_2", "Wright_2019")
 austraits_lite <- extract_dataset(austraits_all, dataset_id)
 
 # updated release
-austraits_newrel <- readRDS("ignore/data/austraits/austraits_newrel.rds") 
-attr(austraits_newrel, "class") <- "austraits"
-austraits_newrel$build_info$version <- "4.0.0"
+austraits_newrel <- load_austraits(version = "4.0.0", path = "ignore/data/austraits")
 
 datasets <- c("Crous_2013", "Crous_2019", "Buckton_2019", "Kooyman_2011", "Bloomfield_2018", 
               "Wright_2019", "Westoby_2014", "Vesk_2019", "Leigh_2003", "Prior_2003",
