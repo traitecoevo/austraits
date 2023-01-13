50154e12ea31fc53eceaa28d016b8f65025d12fb
Fonti Kar <f.kar@unsw.edu.au>
2023-01-13 15:53:21 +1100

    #60 fixed imports for pivot_wider, organised tests, currently not working for new version, waiting for austraits.build update

--- tests/testthat/test-trait_pivot_.R
+++ tests/testthat/test-trait_pivot_.R
@@ -1,23 +1,16 @@
-#Pull in data
-austraits <- austraits_lite
-wide_data <- austraits$traits %>% summarise_trait_means() %>% trait_pivot_wider()
+library(purrr)
 
-test_that("function shouldn't complain and throw errors", {
-  expect_silent(austraits$traits %>% summarise_trait_means() %>% trait_pivot_wider())
-})
+austraits <- list(austraits_lite,
+                  austraits_lite_post)
 
-test_that("functions should throw error when provided wrong input", {
-expect_error(trait_pivot_wider(austraits), label = "The austraits object is a list. Try austraits$trait")
-expect_error(trait_pivot_wider(austraits$sites), label = "Relevent column names from trait data frame not found")
-expect_error(trait_pivot_wider(austraits$methods), label = "Relevent column names from trait data frame not found")
-expect_error(trait_pivot_longer(austraits), label = "Cannot gather an already long format dataframe")
-})
-
+# austraits_lite tests
+wide_data <- austraits_lite$traits %>% summarise_trait_means() %>% trait_pivot_wider()
 test_that("input and output are of expected structure", {
-expect_match(class(wide_data), "list")
-expect_equal(class(wide_data), "list")
-expect_equal(wide_data %>% length, 5)
+  expect_type(wide_data, "list")
+  expect_named(wide_data)
+  expect_gt(ncol(wide_data$value), ncol(austraits$traits))
 })
+
 
 test_that("before and after pivots match", {
   #Checking if widened data has the same length as variables that we are spreading
@@ -27,3 +20,28 @@ test_that("before and after pivots match", {
   #Checking the number of columns matches original data after pivoting wide and then back to long again
   expect_equal(ncol(austraits$traits), ncol(trait_pivot_longer(wide_data)) )
 })
+
+
+# austraits_list and austraits_lite_post
+test_pivot_errors <- function(austraits){
+  test_that("functions should throw error when provided wrong input", {
+    expect_error(trait_pivot_wider(austraits), label = "The austraits object is a list. Try austraits$trait")
+    expect_error(trait_pivot_wider(austraits$taxa), label = "Relevent column names from trait data frame not found")
+    expect_error(trait_pivot_wider(austraits$methods), label = "Relevent column names from trait data frame not found")
+    expect_error(trait_pivot_longer(austraits), label = "Cannot gather an already long format dataframe")
+  })
+}
+
+map(austraits, 
+    test_pivot_errors)
+
+test_that("function shouldn't complain and throw errors", {
+  expect_visible(austraits_lite$traits %>% summarise_trait_means() %>% trait_pivot_wider())
+  # expect_visible(austraits_lite_post$traits %>% trait_pivot_wider())
+})
+
+# austraits_lite_post
+# test_that("structure after pivoting", {
+#   expect_gt(ncol(austraits_lite_post$traits %>% trait_pivot_wider()), ncol(austraits_lite_post$traits))
+#   expect_lt(nrow(austraits_lite_post$traits %>% trait_pivot_wider()), nrow(austraits_lite_post$traits))
+# })
