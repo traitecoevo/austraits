9a9be5852f70bc61b715118b8a86834a4f3132a2
ehwenk <ehwenk@gmail.com>
2024-09-04 10:10:00 +1000

    Update test-check_compatibiity.R
    one last test.

--- tests/testthat/test-check_compatibiity.R
+++ tests/testthat/test-check_compatibiity.R
@@ -5,6 +5,7 @@ test_check_compatibility <- function(austraits){
   test_that("check_compatibiity successfully detects if the dataframe is compatible with austraits 3.0", {
     expect_silent(check_compatibility(austraits_5.0.0_lite))
     expect_true(check_compatibility(austraits_5.0.0_lite))
+    expect_output(check_compatibility(austraits_3.0.2_lite))
     expect_false(check_compatibility(austraits_3.0.2_lite))
   })
 }
