50bd34f09c0cef3bb0d3a2d1d04fb1cea6a7d521
Fonti Kar <f.kar@unsw.edu.au>
2024-09-05 16:50:58 +1000

    Fixed tests that used extract

--- tests/testthat/test-trait_bind_sep.R
+++ tests/testthat/test-trait_bind_sep.R
@@ -1,11 +1,8 @@
-#Pull in data
-austraits <- austraits_3.0.2_lite
-
 #Extract a dataset
 dataset_id <- c("Falster_2005_2")
-subset <- extract_dataset(austraits, dataset_id = dataset_id)
+subset <- extract_dataset(austraits_5.0.0_lite, dataset_id = dataset_id)
 bounded <- bind_trait_values(subset$traits)
-seperated <-separate_trait_values(data = bounded, austraits$definitions)
+seperated <-separate_trait_values(data = bounded, austraits_5.0.0_lite$definitions)
 
 test_that("binding/seperating was successful", {
   expect_true(grep("--", bounded$value) %>% any())
@@ -23,6 +20,6 @@ test_that("structure of dataframes is what we expect", {
 
 test_that("Errors with incorrect argument inputs", {
   expect_error(bind_trait_values(subset), label = "Input is a list, not trait dataframe")
-  expect_error(bind_trait_values(austraits), label = "Input is a list, not trait dataframe")
-  expect_error(separate_trait_values(austraits), label = "Input is a list, not bounded dataframe")
+  expect_error(bind_trait_values(austraits_5.0.0_lite), label = "Input is a list, not trait dataframe")
+  expect_error(separate_trait_values(austraits_5.0.0_lite), label = "Input is a list, not bounded dataframe")
 })
