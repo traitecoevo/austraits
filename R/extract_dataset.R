85beb0a90b2661e5f291b376ae8ecd9682e3daf9
Fonti Kar <f.kar@unsw.edu.au>
2024-09-05 10:01:50 +1000

    Test deprecate for extract_dataset

--- R/extract_dataset.R
+++ R/extract_dataset.R
@@ -15,6 +15,10 @@
 
 
 extract_dataset <- function(austraits, dataset_id) {
+  
+  # Check compatability
+  status <- check_compatibility(austraits)
+  
   # Switch for different versions
   version <- what_version(austraits)
   
@@ -31,36 +35,42 @@ extract_dataset <- function(austraits, dataset_id) {
 
 #' @title Extract specific dataset from austraits object for versions <=3.0.2
 #' @rdname extract_dataset
+#' @description `r lifecycle::badge("deprecated")`
 extract_dataset1 <- function(austraits, dataset_id){
-  austraits$taxonomic_updates <-
-    tidyr::separate_rows(austraits$taxonomic_updates, dataset_id, sep=" ")
   
-  ret <- list()
-  for(v in c("traits", "sites", "contexts", "methods", "excluded_data", "taxonomic_updates",  "contributors"))
-    ret[[v]] <- austraits[[v]][ austraits[[v]][["dataset_id"]] %in% dataset_id,]
-  # NB: can't use dplyr::filter in the above as it doesn't behave when the variable name is the same as a column name
+  .Deprecated(msg = paste("'extract_dataset' is deprecated in version", 
+                          packageVersion("austraits"),
+                          "of austraits."))
   
-  ret[["taxa"]] <- austraits[["taxa"]] %>% dplyr::filter(taxon_name %in% ret[["traits"]][["taxon_name"]])
-  
-  ret[["definitions"]] <- austraits[["definitions"]]
-  ret[["build_info"]] <- austraits[["build_info"]]
-  
-  keys <- dplyr::union(ret$methods$source_primary_key, 
-                       ret$methods$source_secondary_key %>% strsplit("; ") %>% unlist()) %>% 
-    unique() %>% stats::na.omit() %>% as.character()
-  
-  ret[["sources"]] <- austraits$sources[keys]
-  
-  ret[["sources"]] <- austraits[["sources"]][keys]
-  
-  assertthat::are_equal(sort(names(austraits)), sort(names(ret)))
-  
-  ret[names(austraits)]
-  
-  # Assign class
-  attr(ret, "class") <- "austraits"
-  
-  ret
+  # austraits$taxonomic_updates <-
+  #   tidyr::separate_rows(austraits$taxonomic_updates, dataset_id, sep=" ")
+  # 
+  # ret <- list()
+  # for(v in c("traits", "sites", "contexts", "methods", "excluded_data", "taxonomic_updates",  "contributors"))
+  #   ret[[v]] <- austraits[[v]][ austraits[[v]][["dataset_id"]] %in% dataset_id,]
+  # # NB: can't use dplyr::filter in the above as it doesn't behave when the variable name is the same as a column name
+  # 
+  # ret[["taxa"]] <- austraits[["taxa"]] %>% dplyr::filter(taxon_name %in% ret[["traits"]][["taxon_name"]])
+  # 
+  # ret[["definitions"]] <- austraits[["definitions"]]
+  # ret[["build_info"]] <- austraits[["build_info"]]
+  # 
+  # keys <- dplyr::union(ret$methods$source_primary_key, 
+  #                      ret$methods$source_secondary_key %>% strsplit("; ") %>% unlist()) %>% 
+  #   unique() %>% stats::na.omit() %>% as.character()
+  # 
+  # ret[["sources"]] <- austraits$sources[keys]
+  # 
+  # ret[["sources"]] <- austraits[["sources"]][keys]
+  # 
+  # assertthat::are_equal(sort(names(austraits)), sort(names(ret)))
+  # 
+  # ret[names(austraits)]
+  # 
+  # # Assign class
+  # attr(ret, "class") <- "austraits"
+  # 
+  # ret
 }
 
 
