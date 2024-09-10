ecb3da7b091c74d54bf3df8eb33c8fac4913c320
Elizabeth Wenk <ehwenk@gmail.com>
2024-09-10 11:03:11 +1000

    update documentation for traits_pivot_wider & traits_pivot_longer

--- R/trait_pivot_longer.R
+++ R/trait_pivot_longer.R
@@ -1,16 +1,20 @@
 #' @title Pivot wide format AusTrait data into a long format
 #'
 #' @description trait_pivot_longer "gathers" wide format data into a "tidy" format
-#' This function converts the data into long format where observations are on different rows and the type of observation is denoted by trait name.
+#' This function converts the data into long format where measurements are on different rows and the type of observation is denoted by trait name.
 #' In other words, trait_pivot_longer reverts the actions of trait_pivot_wider
-#' @param wide_data output from trait_pivot_wider. For <= v3.0.2 list object containing wide data generated,For > v3.0.2 a tibble of wide data  
+#' @param wide_data output from trait_pivot_wider.
 #' @return A tibble in long format
 #' @details 
-#' - If `bind_trait_values` or `summarise_trait_means` was applied prior to `trait_pivot_wider` for AusTraits
-#' <= v3.0.2, `trait_pivot_longer` will return a tibble with fewer observations than the original traits table. 
-#' - For AusTraits version >3.0.2,  `trait_pivot_longer` will return a tibble with fewer columns than that original traits table
-#'    - The excluded columns include: "unit", "replicates", "measurement_remarks", "basis_of_record", "basis_of_value"
-#' 
+#' trait_pivot_wider has been developed to extract datasets from databases built using the traits.build workflow. 
+#' Learn more at:
+#'   https://github.com/traitecoevo/traits.build &
+#'   https://github.com/traitecoevo/traits.build-book
+#'
+#' Note to AusTraits users:
+#' -  This function works with AusTraits version >= 5.0.0 (from Nov 2023 release)
+#' -  For AusTraits versions <= 4.2.0 (up to Sept 2023 release) see https://github.com/traitecoevo/austraits for how to install old versions of the package or download a newer version of the database."
+#'
 #' @examples 
 #' \dontrun{
 #' data <- austraits$traits %>% 
