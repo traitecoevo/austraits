8989076d9bc17c38835ace3e08d64baccd719432
Fonti Kar <f.kar@unsw.edu.au>
2024-09-12 12:51:14 +1000

    Removed switch for plot_locations, refactored to accept both traits.build and traits table and updated tests #91

--- R/plot_locations.R
+++ R/plot_locations.R
@@ -19,75 +19,28 @@
 
 
 plot_locations <- function(aus_traits, feature="trait_name", ...){
-  # Setting up
-  ## Determine version using col names of traits table
-  if(any(stringr::str_detect(names(aus_traits), "location"))){
-    version = "newer"
-  } else(
-    version = "older"
-  )
-
-  switch (version,
-          'newer' = plot_locations2(aus_traits, feature),
-          'older' = plot_locations1(aus_traits, feature)
-          )
-}
-
-#' Location plot for AusTraits versions <= 3.0.2
-#' @noRd
-plot_locations1 <- function(aus_traits, feature, ...){
-  au_map <- australia_map_raster %>%
-    dplyr::mutate(australia = as.factor(australia))
   
+  # Check if traits.build object or the traits table
+  # If traits.build, check if traits table contains coordinate cols
   if( is.null(dim(aus_traits)) ){
-    traits <- aus_traits$traits
-  } else{
-    traits <- aus_traits
+    
+    # Extract traits table if needed
+    traits <- get_traits_table(aus_traits)
+    
+    if( length(stringr::str_which(names(traits), "(deg)")) < 2 ){
+      cli::cli_alert_info("Coordinate columns were not detected, joining location tables now.")
+      aus_traits <- aus_traits |> join_locations()
+      traits <- get_traits_table(aus_traits)
+    }
+  } else {
+    traits <- aus_traits #If not traits.build, assign traits table as traits
+    
+    # Check if traits contains coordinate cols in traits table
+    if( length(stringr::str_which(names(traits), "(deg)")) < 2 ) 
+      cli::cli_abort("No location data found in traits table - try `join_locations()` first before `plot_locations()`")
   }
-  #Create site data
-  sites <- 
-    traits %>%
-    dplyr::select(site_name, `latitude (deg)`, `longitude (deg)`, !!feature) %>%
-    tidyr::drop_na() %>%
-    dplyr::mutate(dplyr::across(c("longitude (deg)","latitude (deg)"), as.numeric)) %>% 
-    dplyr::filter(
-      `latitude (deg)` > (-45), `latitude (deg)` < (-9.5),
-      `longitude (deg)` > (110), `longitude (deg)` < (153))
   
-  #Create site map  
-  site_map <- 
-    ggplot2::ggplot() +
-    ggplot2::geom_raster(data = au_map, ggplot2::aes(x = x, y = y, fill = australia), show.legend = FALSE) +
-    # Add trait data
-    ggpointdensity::geom_pointdensity(
-      data = sites,
-      ggplot2::aes(y = `latitude (deg)`, x = `longitude (deg)`),
-      inherit.aes = FALSE,
-      show.legend = TRUE,
-      adjust = 1,
-      ...
-    ) +
-    ggplot2::scale_x_continuous(limits = c(NA, 154)) +
-    ggplot2::scale_fill_manual(values = "cadetblue4", na.value="white", guide = "none") +
-    viridis::scale_color_viridis(option = "plasma") +
-    ggplot2::theme(
-      legend.justification = c(-0.1, 0),
-      legend.position = "bottom",
-      legend.direction  = "horizontal",
-      panel.grid.major = ggplot2::element_blank(),
-      panel.grid.minor = ggplot2::element_blank(),
-      panel.background = ggplot2::element_blank(),
-      panel.border = ggplot2::element_rect(colour = "black", fill=NA, linewidth=1),
-      axis.ticks.length = ggplot2::unit(1, "mm"),
-      axis.ticks = ggplot2::element_line(linewidth = 1)
-    )  + ggplot2::xlab("") + ggplot2::ylab("") + 
-    ggplot2::coord_fixed()
-  
-  # facet by feature if specified - default
-  if(!is.na(feature)){
-    site_map <- site_map + ggplot2::facet_wrap(paste("~", feature))
-  }
-  suppressWarnings(print(site_map))
+  plot_locations2(traits, feature)
 }
 
 #' Location plot for AusTraits versions > 3.0.2
@@ -96,17 +49,11 @@ plot_locations2 <- function(aus_traits, feature, ...){
   au_map <- australia_map_raster %>%
     dplyr::mutate(australia = as.factor(australia))
   
-  if( is.null(dim(aus_traits)) ){
-  traits <- aus_traits$traits
-  } else{
-    traits <- aus_traits
-  }
-  
   #Create site data
-  sites <- 
-    traits %>%
-    dplyr::select(location_name, `latitude (deg)`, `longitude (deg)`, !!feature) %>%
-    tidyr::drop_na() %>%
+  sites <-
+    aus_traits |> 
+    dplyr::select(!!feature, tidyselect::any_of(c("site_name", "location_name", "latitude (deg)", "longitude (deg)"))) |> 
+    tidyr::drop_na() |> 
     dplyr::mutate(dplyr::across(c("longitude (deg)","latitude (deg)"), as.numeric)) %>% 
     dplyr::filter(
       `latitude (deg)` > (-45), `latitude (deg)` < (-9.5),
@@ -162,7 +109,19 @@ plot_locations2 <- function(aus_traits, feature, ...){
 #' @export
 
 plot_site_locations <- function(traits, feature="trait_name", ...){
-  .Deprecated("plot_locations")
+  # Extract function name
+  function_name <- as.character(sys.calls()[[1]])[1]
   
-  plot_locations1(traits, feature="trait_name", ...)
+  # Determine if traits table or traits.build object
+  if( is.null(dim(traits))){
+    # Extract AusTraits version
+    AusTraits_version <- print_version(aus_traits)
+  } else
+    AusTraits_version <- "< 5.0.0"
+  
+  cli::cli_abort(c(
+    "i" = "{function_name} has been deprecated!",
+    ">" =  "Use plot_locations() instead"
+  )
+  )
 }
