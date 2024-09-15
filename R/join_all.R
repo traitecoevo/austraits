9e62764ed4042a2b716acaa8c9762610e4a7521f
Elizabeth Wenk <ehwenk@gmail.com>
2024-09-16 08:13:29 +1000

    Update join_all.R
    partial edits to make `join_` functions usable within `database_create_combined_table`.

--- R/join_all.R
+++ R/join_all.R
@@ -5,7 +5,7 @@
 #' 
 #' @param austraits traits.build generated database
 #' @param vars vector specifying which columns from a specific relational table to join to the traits table (works only for `join_` functions joining a single dataframe)
-#' @param collapse_context for `join_contexts` only. Logical, whether contexts for a given observation will be concatenated into one cell
+#' @param collapse_context for `join_context_properties` and `join_location_properties` only. Logical, whether location properties or context properties for a given observation will be concatenated into one cell
 #' @return traits.build list object, but with additional fields (columns) appended to `traits` dataframe
 #' @details
 #' the `join_` functions have been developed to join relational tables for databases built using the traits.build workflow. 
@@ -23,8 +23,11 @@
 #' \dontrun{
 #' austraits$traits
 #' 
-#' #Append locations data
+#' #Append locations properties and output a single table
 #' (austraits %>% join_locations)$traits
+#' 
+#' #Append location properties, but maintain the relational database
+#' austraits %>% join_locations()
 #'
 #' #Append contexts
 #' (austraits %>% join_contexts)$traits
@@ -70,6 +73,12 @@ join_taxonomy <- function(austraits, vars =  c("family", "genus", "taxon_rank", 
     function_not_supported(austraits)
   } 
   
+  vars <- as.vector(vars)
+  
+  if (vars[1] == "all" & length(vars == 1)){
+    vars <- names(austraits$taxa)
+  }
+  
   austraits$traits <- austraits$traits %>%
     dplyr::left_join(by="taxon_name", austraits$taxa %>% dplyr::select("taxon_name", tidyselect::any_of(vars)))
   
@@ -81,14 +90,18 @@ join_taxonomy <- function(austraits, vars =  c("family", "genus", "taxon_rank", 
 #' @export
 #' @rdname join_all
 
-join_methods <- function(austraits, vars =  c("methods", "year_collected_start", "year_collected_end", "collection_type")) {
+join_methods <- function(austraits, vars =  c("methods")) {
   # Check compatability
   status <- check_compatibility(austraits)
   
   # If compatible
   if(!status){
     function_not_supported(austraits)
-  } 
+  }
+
+  if (vars == "all"){
+    vars <- names(austraits$methods)
+  }
 
   austraits$methods %>% 
     dplyr::select(c("dataset_id", "trait_name", "method_id"), tidyselect::any_of(vars)) %>% 
@@ -101,20 +114,60 @@ join_methods <- function(austraits, vars =  c("methods", "year_collected_start",
   austraits
 }
 
-#' @title Joining location information to traits table
+#' @title Joining location coordinates to traits table
 #' @export
 
 #' @rdname join_all
-
-join_locations <- function(austraits, vars =  c("longitude (deg)","latitude (deg)")) {
+join_location_coordinates <- function(austraits) {
   # Check compatability
   status <- check_compatibility(austraits)
   
   # If compatible
   if(!status){
     function_not_supported(austraits)
-  } 
+  }
+
+  location_coordinates <-
+    database$locations %>%
+    dplyr::filter(location_property %in% c("latitude (deg)", "longitude (deg)")) %>%
+    tidyr::pivot_wider(names_from = location_property, values_from = value)
+
+  if (any(stringr::str_detect(names(location_coordinates), "latitude "))) {
+    austraits$traits <- austraits$traits %>%
+    dplyr::left_join(by=c("dataset_id", "location_id"), location_coordinates)
+
+  } else {
+    austraits$traits <- austraits$traits %>%
+      dplyr::mutate(
+        location_name = NA_character_,
+        `latitude (deg)` = NA_character_,
+        `longitude (deg)` = NA_character_,
+      )
+  }
+
+  austraits
+}
+
+#' @title Joining location properties to traits table
+#' @export
+
+#' @rdname join_all
+
+join_location_properties <- function(austraits, vars =  "all") {
+  # Check compatability
+  status <- check_compatibility(austraits)
+
+  # If compatible
+  if(!status){
+    function_not_supported(austraits)
+  }
   
+  if (vars == "all") {
+    austraits$locations %>%
+    distinct(location_properties) %>%
+    filter(!location_properties %in% "latitude (deg)", "longitude (deg)")
+  }
+
   sites <- 
     austraits$locations %>% 
     dplyr::filter(location_property %in%  vars) %>% 
@@ -126,75 +179,5 @@ join_locations <- function(austraits, vars =  c("longitude (deg)","latitude (deg
   austraits
 }
 
-#' @title  Joining location info for AusTraits versions <= 3.0.2
-#' @description `r lifecycle::badge('deprecated')`
-#' Joining location info for AusTraits versions <= 3.0.2
-#' @param austraits austraits object
-#' @param vars  variables from site table to join 
-#' @export
-
-join_sites <- function(austraits, vars =  c("longitude (deg)","latitude (deg)")) {
-  function_not_supported(austraits)
-}
-
-#' @export
-#' @rdname join_all
-
-join_contexts <- function(austraits, collapse_context = FALSE){
-  # Check compatability
-  status <- check_compatibility(austraits)
-  
-  # If compatible
-  if(!status){
-    function_not_supported(austraits)
-  } 
-
-  traits2 <- split(austraits$traits, austraits$traits$dataset_id)
-  contexts2 <- split(austraits$contexts, austraits$contexts$dataset_id)
-  
-  traits_vars <- names(austraits$traits)
-
-  problem_studies <- c("Hall_1981")
-
-  for(id in names(traits2)) {
-  
-    if(!is.null(contexts2[[id]][1]) & ! (id %in% problem_studies)) {
-    
-      context_ids <- 
-        unique(contexts2[[id]]$link_id)
-  
-      for(v in context_ids[!is.na(context_ids)]) {
-        
-        context_sub <- 
-          contexts2[[id]] %>%
-          dplyr::select(-dplyr::any_of(c("category", "description"))) %>%
-          dplyr::filter(link_id == v) %>% 
-          tidyr::separate_rows(link_vals) %>% 
-          tidyr::pivot_wider(values_from = value, names_from = context_property) %>%
-          tidyr::pivot_wider(names_from = link_id, values_from = link_vals)
-        
-        traits2[[id]] <- 
-          dplyr::left_join(by = c("dataset_id", v),
-                    traits2[[id]],
-                    context_sub
-          )
-      }
-      
-      if(collapse_context == TRUE){
-        context_text <-
-          traits2[[id]] %>% 
-          dplyr::select(-dplyr::any_of(traits_vars)) %>% collapse_cols()
-
-        traits2[[id]] <- traits2[[id]] %>% 
-          dplyr::mutate(context = context_text) %>% 
-          dplyr::select(dplyr::any_of(traits_vars), context)
-      }
-    }
-  }
-
-  austraits$traits <- traits2 %>% dplyr::bind_rows()
-
-  austraits
-}
 
 
