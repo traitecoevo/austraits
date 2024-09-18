79512002414fd03985326c5241973dfb32deba84
ehwenk <ehwenk@gmail.com>
2024-09-18 14:21:47 +1000

    Add new lookup_ functions for context_properties, location_properties
    Add new lookup_ functions for context_properties, location_properties that output vectors of property values that can be used within the join_ functions

---
+++ tests/testthat/test-lookup_.R
@@ -0,0 +1,16 @@
+# test lookup_ functions
+
+test_that("the lookup_ functions are working as intended", {
+
+  expect_gt(length(austraits_5.0.0_lite %>% lookup_trait("leaf")), 100)
+  expect_error(austraits_5.0.0_lite %>% lookup_trait("this is not a trait"))
+  
+  expect_gt(length(austraits_5.0.0_lite %>% lookup_location_property("soil")), 10)
+  expect_vector(austraits_5.0.0_lite %>% lookup_location_property("precipitation"))
+  expect_error(austraits_5.0.0_lite %>% lookup_location_property("this is not a location property"))
+  
+  expect_gt(length(austraits_5.0.0_lite %>% lookup_context_property("temperature")), 2)
+  expect_vector(austraits_5.0.0_lite %>% lookup_context_property("season"))
+  expect_error(austraits_5.0.0_lite %>% lookup_context_property("this is not a context property"))
+
+})
