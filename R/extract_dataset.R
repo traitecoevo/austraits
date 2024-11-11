6858990c53de8ffe5602f5ea7765f104c662d429
Fonti Kar <f.kar@unsw.edu.au>
2024-11-11 12:46:25 +1100

    Updating links in function docs, added Lizzy as contributor, added branch for CI #110

--- R/extract_dataset.R
+++ R/extract_dataset.R
@@ -9,12 +9,12 @@
 #' @details
 #' `extract_dataset` has been developed to extract data for specific datasets from databases built using the traits.build workflow. 
 #' Learn more at:
-#'   [https://github.com/traitecoevo/traits.build] &
-#'   [https://github.com/traitecoevo/traits.build-book]
+#'   [https://github.com/traitecoevo/traits.build](https://github.com/traitecoevo/traits.build) &
+#'   [https://github.com/traitecoevo/traits.build-book](https://github.com/traitecoevo/traits.build-book)
 #'
 #' Note to AusTraits users:
 #' -  This function works with AusTraits version >= 5.0.0 (from Nov 2023 release)
-#' -  For AusTraits versions <= 4.2.0 (up to Sept 2023 release) see [https://github.com/traitecoevo/austraits] for how to install old versions of the package or download a newer version of the database."
+#' -  For AusTraits versions <= 4.2.0 (up to Sept 2023 release) see [https://github.com/traitecoevo/austraits](https://github.com/traitecoevo/austraits) for how to install old versions of the package or download a newer version of the database."
 #'
 #' @examples
 #' \dontrun{
