047dbb14ddd89c2b592b3dd306194dfb1b21a3a2
Fonti Kar <f.kar@unsw.edu.au>
2024-11-14 14:54:07 +1100

    Using cli altogether

--- R/print.austraits.R
+++ R/print.austraits.R
@@ -8,14 +8,8 @@
 #' @export
 
 print.traits.build <- function(database, ...){
-  # browser()
   
   # Setting up
-  version <- database$build_info$version %>% as.character()
-  nrecords <- nrow(database$traits)
-  nspecies <- unique(database$traits$taxon_name) %>% length()
-  ntraits <- unique(database$traits$trait_name) %>% length()
-  
   database_name <- database$metadata$title
   
   traits.build_version <- at_six$metadata$related_identifiers |> 
@@ -23,35 +17,78 @@ print.traits.build <- function(database, ...){
     dplyr::filter(resource_type == "software") |> 
     dplyr::pull(version)
   
-  # Formulate message
-  cli::cli_h1("This is {database_name}!")
-  
-  cli::cli_bullets(c(
-    "i" = "A database built using traits.build version {traits.build_version}",
-    "i" = "This database contains a total of {nrecords} records, for {nspecies} taxa and {ntraits} traits."
-  )
-  )
+  version <- database$build_info$version %>% as.character()
+  nrecords <- nrow(database$traits)
+  nspecies <- unique(database$traits$taxon_name) %>% length()
+  ntraits <- unique(database$traits$trait_name) %>% length()
+
   
   if(package_version(version) <= '3.0.2'){
+    
+    # Setting up
+    database_name <- database$definitions$austraits$description
   
-  cat("\nThis object is a 'list' with the following components:\n\n",
-  database$definitions$austraits$elements %>%
-  purrr::map(~.x[["description"]]) %>% 
-  as.vector() %>% 
-  sprintf("- `%s`: %s", names(.), .) %>%
-  paste(collapse="\n")
-  )
+    fun <- function() {
+      cli::cli_h1("This is {version} of {database_name}!")
+      
+      cli::cli_bullets(c(
+        "i" = "A database built using traits.build version {traits.build_version}",
+        "i" = "This database contains a total of {nrecords} records, for {nspecies} taxa and {ntraits} traits."
+      )
+      )
+      
+      cli::cli_h2("This object is a 'list' with the following components:")
+      cli::cli_div(theme = list(span.emph = list(color = "forestgreen")))
+      cli::cli_ul()
+      cli::cli_li("{.emph traits}: A table containing measurements of traits.")
+      cli::cli_li("{.emph locations}: A table containing observations of location/site characteristics associated with information in `traits`. Cross referencing between the two dataframes is possible using combinations of the variables `dataset_id`, `location_name`.")
+      cli::cli_li("{.emph contexts}: A table containing observations of contextual characteristics associated with information in `traits`. Cross referencing between the two dataframes is possible using combinations of the variables `dataset_id`, `link_id`, and `link_vals`.")
+      cli::cli_li("{.emph methods}: A table containing details on methods with which data were collected, including time frame and source. Cross referencing with the `traits` table is possible using combinations of the variables `dataset_id`, `trait_name`.")
+      cli::cli_li("{.emph excluded_data}: A table of data that did not pass quality test and so were excluded from the master dataset.")
+      cli::cli_li("{.emph taxonomic_updates}: A table of all taxonomic changes implemented in the construction of AusTraits. Changes are determined by comapring against the APC (Australian Plant Census) and APNI (Australian Plant Names Index).")
+      cli::cli_li("{.emph taxa}: A table containing details on taxa associated with information in `traits`. This information has been sourced from the APC (Australian Plant Census) and APNI (Australian Plant Names Index) and is released under a CC-BY3 license.")
+      cli::cli_li("{.emph contributors}: A table of people contributing to each study.")
+      cli::cli_li("{.emph sources}: Bibtex entries for all primary and secondary sources in the compilation.")
+      cli::cli_li("{.emph definitions}: A copy of the definitions for all tables and terms. Information included here was used to process data and generate any documentation for the study.")
+      cli::cli_li("{.emph build_info}:  A description of the computing environment used to create this version of the dataset, including version number, git commit and R session_info.")
+      cli::cli_end()
+      
+      cli::cli_alert_info("To access a component, try using the $ e.g. austraits$traits")
+    }
+    
+    fun()
   } else{
-    cat("\nThis object is a 'list' with the following components:\n\n",
-        database$schema$austraits$elements %>%
-          purrr::map(~.x[["description"]]) %>% 
-          as.vector() %>% 
-          sprintf("- `%s`: %s", names(.), .) %>%
-          paste(collapse="\n")
-    )
+    fun <- function() {
+            cli::cli_h1("This is {version} of {database_name}!")
+      
+      cli::cli_bullets(c(
+        "i" = "A database built using traits.build version {traits.build_version}",
+        "i" = "This database contains a total of {nrecords} records, for {nspecies} taxa and {ntraits} traits."
+      )
+      )
+      
+      cli::cli_h2("This object is a 'list' with the following components:")
+      cli::cli_div(theme = list(span.emph = list(color = "forestgreen")))
+      cli::cli_ul()
+      cli::cli_li("{.emph traits}: A table containing measurements of traits.")
+      cli::cli_li("{.emph locations}: A table containing observations of location/site characteristics associated with information in `traits`. Cross referencing between the two dataframes is possible using combinations of the variables `dataset_id`, `location_name`.")
+      cli::cli_li("{.emph contexts}: A table containing observations of contextual characteristics associated with information in `traits`. Cross referencing between the two dataframes is possible using combinations of the variables `dataset_id`, `link_id`, and `link_vals`.")
+      cli::cli_li("{.emph methods}: A table containing details on methods with which data were collected, including time frame and source. Cross referencing with the `traits` table is possible using combinations of the variables `dataset_id`, `trait_name`.")
+      cli::cli_li("{.emph excluded_data}:   A table of data that did not pass quality test and so were excluded from the master dataset.")
+      cli::cli_li("{.emph taxa}:  A table containing details on taxa associated with information in `traits`. This information has been sourced from the APC (Australian Plant Census) and APNI (Australian Plant Names Index) and is released under a CC-BY3 license.")
+      cli::cli_li("{.emph definitions}:  A copy of the definitions for all tables and terms. Information included here was used to process data and generate any documentation for the study.")
+      cli::cli_li("{.emph sources}:  Bibtex entries for all primary and secondary sources in the compilation.")
+      cli::cli_li("{.emph contributors}:  A table of people contributing to each study.")
+      cli::cli_li("{.emph taxonomic_updates}:   table of all taxonomic changes implemented in the construction of AusTraits. Changes are determined by comapring against the APC (Australian Plant Census) and APNI (Australian Plant Names Index).")
+      cli::cli_li("{.emph metadata}: Metadata associated with the dataset, including title, creators, license, subject, funding sources.")
+      cli::cli_li("{.emph build_info}:  A description of the computing environment used to create this version of the dataset, including version number, git commit and R session_info.")
+      cli::cli_end()
+      
+      cli::cli_alert_info("To access a component, try using the $ e.g. austraits$traits")
+    }
+    
+    fun()
   }
-
-  cat("\n\nTo access a component, try using the $ e.g. austraits$traits")  
 }
 
 
